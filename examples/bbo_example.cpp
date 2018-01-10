037309bda0a8c973e06765216ceb4a9f55be47e4
medrimonia <medrimonia@gmail.com>
2018-01-10 09:19:38 +0100

    Adding an example of blackbox optimization with json file

---
+++ examples/bbo_example.cpp
@@ -0,0 +1,44 @@
+#include "rosban_bbo/optimizer_factory.h"
+
+#include "rosban_random/tools.h"
+
+#include <iostream>
+#include <cstdlib>
+
+
+int main(int argc, char ** argv)
+{
+  std::string optimizer_path;
+  if (argc < 2) {
+    std::cerr << "Usage: " << argv[0] << " <optimizer_path>" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  optimizer_path = argv[1];
+  
+  // A simple function to optimize
+  double expected_optimum = 1;
+  rosban_bbo::Optimizer::RewardFunc reward_function = [expected_optimum]
+    (const Eigen::VectorXd & params,std::default_random_engine * engine) -> double
+    {
+      std::normal_distribution<double> noise_distrib(0,1);
+      double x = params(0);
+      double noise = noise_distrib(*engine);
+      return noise - (x-expected_optimum) * (x-expected_optimum);
+    };
+
+  // Initializing guess and parameter space
+  Eigen::Matrix<double,1,2> param_space;
+  param_space << -5, 5;
+  
+  // Loading an optimizer from a Json file
+  rosban_bbo::OptimizerFactory f;
+  std::unique_ptr<rosban_bbo::Optimizer> optimizer = f.buildFromJsonFile(optimizer_path);
+  optimizer->setLimits(param_space);
+
+  // Optimizing
+  std::default_random_engine * engine = rosban_random::newRandomEngine();
+  Eigen::VectorXd best_params = optimizer->train(reward_function, engine);
+
+  std::cout << "Best params: " << best_params.transpose() << std::endl;
+  
+}
--- src/rosban_bbo/optimizer.cpp
+++ src/rosban_bbo/optimizer.cpp
@@ -5,6 +5,9 @@ namespace rosban_bbo
 
 Eigen::VectorXd Optimizer::train(RewardFunc & reward,
                                  std::default_random_engine * engine) {
+  if (limits.rows() == 0) {
+    throw std::logic_error("rosban_bbo::Optimizer: limits of the optimizer have not been initialized");
+  }
   Eigen::VectorXd initial_candidate = (limits.col(0) + limits.col(1)) / 2;
   return train(reward, initial_candidate, engine);
 }
