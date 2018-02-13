c6bc48cd8c271d0e9e88ffb6af18824e20d606d2
medrimonia <medrimonia@gmail.com>
2018-02-13 14:46:00 +0100

    Adding tests based on SinProd and SinSum

--- examples/bbo_example.cpp
+++ examples/bbo_example.cpp
@@ -65,12 +65,114 @@ public:
   double stddev;
 };
 
+/// A sum of sinus according different dimensions
+/// result(x) = sum_{i\in {1,..., D}} sin(2*pi*x_i / periods(i) + offsets(i))
+class SinSum : public OptimizationTask {
+public:
+
+  double sampleReward(const Eigen::VectorXd & parameters,
+                      std::default_random_engine * engine) const {
+    if (parameters.rows() != input_limits.rows() ||
+        parameters.rows() != periods.rows() ||
+        parameters.rows() != offsets.rows()) {
+      throw std::logic_error("SinSum::sampleReward: Size mismatch");
+    }
+    // Computing basic result
+    Eigen::VectorXd tmp = 2 * M_PI * parameters.cwiseProduct(periods.cwiseInverse()) + offsets;
+    double deterministic_part = 0;
+    for (int dim = 0; dim < tmp.rows(); dim++) {
+      deterministic_part += sin(tmp(dim));
+    }
+    // Adding noise
+    std::normal_distribution<double> noise_distrib(0,1);
+    double noise = noise_distrib(*engine);
+    return noise + deterministic_part;
+  }
+
+  Json::Value toJson() const override {
+    Json::Value v = OptimizationTask::toJson();
+    v["periods"] = rhoban_utils::vector2Json(periods);
+    v["offsets"] = rhoban_utils::vector2Json(offsets);
+    v["stddev"] = stddev;
+    return v;
+  }
+
+  void fromJson(const Json::Value & v, const std::string & dir_name) override {
+    (void) dir_name;
+    OptimizationTask::fromJson(v, dir_name);
+    rhoban_utils::tryRead(v, "periods", &periods);
+    rhoban_utils::tryRead(v, "offsets", &offsets);
+    rhoban_utils::tryRead(v, "stddev", &stddev);
+  }
+
+  std::string getClassName() const override {
+    return "SinSum";
+  }
+
+  Eigen::VectorXd periods;
+  Eigen::VectorXd offsets;
+  double stddev;
+};
+
+/// A sum of sinus according different dimensions
+/// result(x) = prod_{i\in {1,..., D}} sin(2*pi*x_i / periods(i) + offsets(i))
+class SinProd : public OptimizationTask {
+public:
+
+  double sampleReward(const Eigen::VectorXd & parameters,
+                      std::default_random_engine * engine) const {
+    if (parameters.rows() != input_limits.rows() ||
+        parameters.rows() != periods.rows() ||
+        parameters.rows() != offsets.rows()) {
+      throw std::logic_error("SinProd::sampleReward: Size mismatch");
+    }
+    // Computing basic result
+    Eigen::VectorXd tmp = 2 * M_PI * parameters.cwiseProduct(periods.cwiseInverse()) + offsets;
+    double deterministic_part = 1;
+    for (int dim = 0; dim < tmp.rows(); dim++) {
+      deterministic_part *= sin(tmp(dim));
+    }
+    // Adding noise
+    std::normal_distribution<double> noise_distrib(0,1);
+    double noise = noise_distrib(*engine);
+    return noise + deterministic_part;
+  }
+
+  Json::Value toJson() const override {
+    Json::Value v = OptimizationTask::toJson();
+    v["periods"] = rhoban_utils::vector2Json(periods);
+    v["offsets"] = rhoban_utils::vector2Json(offsets);
+    v["stddev"] = stddev;
+    return v;
+  }
+
+  void fromJson(const Json::Value & v, const std::string & dir_name) override {
+    (void) dir_name;
+    OptimizationTask::fromJson(v, dir_name);
+    rhoban_utils::tryRead(v, "periods", &periods);
+    rhoban_utils::tryRead(v, "offsets", &offsets);
+    rhoban_utils::tryRead(v, "stddev", &stddev);
+  }
+
+  std::string getClassName() const override {
+    return "SinProd";
+  }
+
+  Eigen::VectorXd periods;
+  Eigen::VectorXd offsets;
+  double stddev;
+};
+
 class TaskFactory : public rhoban_utils::Factory<OptimizationTask> {
 public:
   TaskFactory() {
     
   registerBuilder("MSEOptimization",
                   []() { return std::unique_ptr<OptimizationTask>(new MSEOptimization); });
+  registerBuilder("SinSum",
+                  []() { return std::unique_ptr<OptimizationTask>(new SinSum); });
+  registerBuilder("SinProd",
+                  []() { return std::unique_ptr<OptimizationTask>(new SinProd); });
   }
 };
 
@@ -103,5 +205,14 @@ int main(int argc, char ** argv)
   Eigen::VectorXd best_params = optimizer->train(reward_function, engine);
 
   std::cout << "Best params: " << best_params.transpose() << std::endl;
-  
+
+  int nb_validations = 1000;
+  double total_reward = 0;
+  for (int i = 0; i < nb_validations; i++) {
+    total_reward += task->sampleReward(best_params, engine);
+  }
+  double avg_reward = total_reward / nb_validations;
+  std::cout << "\t-> avg reward: " << avg_reward << std::endl;
+
+  delete(engine);
 }
